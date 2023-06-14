a79b63902738f15286cba97a293e8e98f9ba27f4
Devvaru <syeraru@gmail.com>
2023-06-14 01:28:22 -0600

    Set up event listener for user input

--- play.js
+++ play.js
@@ -1,2 +1,20 @@
 const { connect } = require("./client");
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); // event listener for stdin
+  return stdin;
+};
+
+// "data" callback handler for stdin
+const handleUserInput = function () {
+
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
