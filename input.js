d54632edca1a1b8d13d691c6638c3b58b834be7e
Devvaru <syeraru@gmail.com>
2023-06-14 02:23:16 -0600

    Add wasd input

--- input.js
+++ input.js
@@ -1,5 +1,10 @@
+const { stdin } = require("process");
+
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,11 +14,25 @@ const setupInput = function () {
 };
 
 // "data" callback handler for stdin
-const handleUserInput = function () {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  };
+const handleUserInput = function (key) {
+
+  switch (key) {
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
+    case '\u0003': // ctrl+c
+      process.exit();
+      break;
+  }
 };
 
 module.exports = {
