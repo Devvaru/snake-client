5b5c191c47c986d1a3d43ec658bd8741f3f649be
Devvaru <syeraru@gmail.com>
2023-06-14 00:57:06 -0600

    Move file content of play to client, and export

--- play.js
+++ play.js
@@ -1,22 +1,2 @@
-const net = require("net");
+const { connect } = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // listens for data incoming from server
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
