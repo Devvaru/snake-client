222ca1ff2591cc1baad49c71c8742c97819c5523
Devvaru <syeraru@gmail.com>
2023-06-14 16:41:56 -0600

    Refactor handleUserInput

--- input.js
+++ input.js
@@ -17,37 +17,17 @@ const setupInput = function (conn) {
 // "data" callback handler for stdin
 const handleUserInput = function (key) {
 
-  switch (key) {
-    case 'w':
-      connection.write(`Move: ${keyMapping["w"]}`);
-      break;
-    case 'a':
-      connection.write(`Move: ${keyMapping["a"]}`);
-      break;
-    case 's':
-      connection.write(`Move: ${keyMapping["s"]}`);
-      break;
-    case 'd':
-      connection.write(`Move: ${keyMapping["d"]}`);
-      break;
-    // messages
-    case '1':
-      connection.write(`Say: ${keyMapping[1]}`);
-      break;
-    case '2':
-      connection.write(`Say: ${keyMapping[2]}`);
-      break;
-    case '3':
-      connection.write(`Say: ${keyMapping[3]}`);
-      break;
-    case '4':
-      connection.write(`Say: ${keyMapping[4]}`);
-      break;
-    // end game
-    case '\u0003': // ctrl+c
-      process.exit();
-      break;
+  // quit game
+  if (key === "x") {
+    process.exit();
   }
+
+  // wasd and message inputs
+  const msg = keyMapping[key];
+  if (msg) {
+    connection.write(msg);
+  }
+  
 };
 
 module.exports = {
