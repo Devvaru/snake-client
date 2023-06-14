85acea51cad9df3572e24a0b53695b481ea90a61
Devvaru <syeraru@gmail.com>
2023-06-14 16:46:07 -0600

    Lint code and add comments

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require("net");
 const { IP, PORT, userName } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   console.log("Connecting ...");
   const conn = net.createConnection({
     host: IP,
@@ -17,14 +17,16 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // events on connect
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write(`Name: ${userName}`);
   });
 
+  // events on error
   conn.on("error", () => {
     console.log("You can't move when you're dead! Quit game to restart.");
-  })
+  });
 
   return conn;
 };
